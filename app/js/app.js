682a81e5460d328a7e08192a8a5d7fbfc42b2def
hien.hothithu <hien.hothithu@jpsys.jp>
2024-06-14 17:25:34 +0900

    Update Japanese Language for Base-ver - 50%

--- app/js/app.js
+++ app/js/app.js
@@ -17981,6 +17981,68 @@ var store = (0,vuex__WEBPACK_IMPORTED_MODULE_0__.createStore)({
           content: "これは、Web アプリ製品でユーザーの問題を解決するテクノロジー ソリューションを提供することに特化した会社です。同時に、同社は独自の製品も作成しています。</p><ul><li>利用可能な Web アプリのメンテナンスと新機能の追加は、顧客から要求されます</li><li>Adobe XD を使用してユーザー インターフェイスを設計し、経験豊富な人々と協力して明確で効果的なデザインを確保し、新しいページを開発し、完璧なピクセル精度を確保することを担当しました。</li><li>また、Vuejs を使用してプロジェクト用の Web アプリを開発しました。</li></ul>",
           technology: "ASP.NET MVC, Vuejs, JavaScript, Knockoutjs, SQLserver HTML, SCSS, CSS"
         }
+      },
+      en_base: {
+        name: 'H I E N &nbsp; H O',
+        job: 'Web/UI Developer',
+        contacts: [{
+          text: "",
+          link: "mailto:"
+        }, {
+          text: "",
+          link: "mailto:"
+        }],
+        description: "I'm Hien (my nickname is Teo), a software developer with 2 years of experience in web development. I specialize in creating a website and creating user-friendly interfaces using C#, SCSS, JavaScript, and Vue.<br><br>I am Vietnamese but currently I live in Japan. I'm trying to find new fun things, and especially looking for more friends. Nice to meet you ^o^<br><br><small style='font-size: 14px'><a style='text-decoration: underline;color: cadetblue;'>This is basic information that can be made public. Please contact me if you want my detailed CV. uwu</a></small>",
+        skills: "ASP.NET, SQL Server, Javascript, Vuejs, SCSS",
+        educations: {
+          educations: [{
+            date: "2015 - 2020",
+            text: "Danang University of Science and Technology"
+          }],
+          certificates: [{
+            date: "2022",
+            text: "Japanese-Language Proficiency Test N2 Certificate"
+          }]
+        },
+        experience: {
+          position: "Software Engineer",
+          company: "Nihon System",
+          date: "2022 - Current",
+          location: "Japan",
+          content: "<p class='pt-10'>Working for a company specializing in providing technology solutions to solve user problems with web app products. At the same time, the company also creates its own products.</p><p>We create web app products, winforms applications that provide tools to meet customer needs such as in the logistics industry, order management, invoices...</p>",
+          technology: "ASP.NET MVC, Vuejs, JavaScript, Knockoutjs, SQLserver, HTML, SCSS, CSS"
+        }
+      },
+      ja_base: {
+        name: 'ヒエン・ホ',
+        job: 'Web/UI開発者',
+        contacts: [{
+          text: "",
+          link: "mailto:"
+        }, {
+          text: "",
+          link: "mailto:"
+        }],
+        description: "I'm Hien (my nickname is Teo), a software developer with 2 years of experience in web development. I specialize in creating a website and creating user-friendly interfaces using C#, SCSS, JavaScript, and Vue.<br><br>I am Vietnamese but currently I live in Japan. I'm trying to find new fun things, and especially looking for more friends. Nice to meet you ^o^<br><br><small style='font-size: 14px'><a style='text-decoration: underline;color: cadetblue;'>This is basic information that can be made public. Please contact me if you want my detailed CV. uwu</a></small>",
+        skills: "ASP.NET, SQL Server, Javascript, Vuejs, SCSS",
+        educations: {
+          educations: [{
+            date: "2015 - 2020",
+            text: "ダナン工科大学"
+          }],
+          certificates: [{
+            date: "2022",
+            text: "日本語能力試験N2"
+          }]
+        },
+        experience: {
+          position: "ソフトウェアエンジニア",
+          company: "日本システム",
+          date: "2022 - 現在",
+          location: "日本",
+          content: "<p class='pt-10'>Working for a company specializing in providing technology solutions to solve user problems with web app products. At the same time, the company also creates its own products.</p><p>We create web app products, winforms applications that provide tools to meet customer needs such as in the logistics industry, order management, invoices...</p>",
+          technology: "ASP.NET MVC, Vuejs, JavaScript, Knockoutjs, SQLserver, HTML, SCSS, CSS"
+        }
       }
     },
     selectedLanguage: 'en' // Ngôn ngữ mặc định
